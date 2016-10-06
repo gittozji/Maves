1020445720bcb587ffcbae91811bd7ccdd488c1b
gittozji <yitozji@163.com>
2016-10-06 15:13:05 +0800

    使用类将其代码封装，运用了单例模式，修改了部分命名

--- waves.js
+++ waves.js
@@ -1,20 +1,30 @@
 $(document).ready(function(){
 
-	$(".waves").mousedown(function(e) {
+	$(".mmd-waves").mousedown(function(e) {
+		var m = new MavesClass();
+		m.showWaves(this,e);
+	});
+});
+//涟漪类，使其相对独立
+function MavesClass(){
+	if(MavesClass.instance !== undefined){
+		return MavesClass.instance;
+	}
+	MavesClass.instance = this;
 	
-		var box = $(this);
-		var wavesDiv = box.find("div");
-		
+	this.showWaves = function(_this,e){
+		box = $(_this);
+		wavesDiv = box.find("div");
 		//第一次没有涟漪div，动态生成
 		if(wavesDiv[0] == null){
-			var div = "<div class='waves-effect'></div>";
+			var div = "<div class='mmd-waves-effect'></div>";
 			box.append(div);
 			wavesDiv = box.find("div");
 		}
 		
-	
+
 		//设置按钮样式为’waves-effect‘即去掉动画样式’waves-effect-animation‘
-		wavesDiv[0].className = 'waves-effect';
+		wavesDiv[0].className = 'mmd-waves-effect';
 		
 		//计算涟漪坐标（折算成左上角坐标而非中心点），涟漪大小（取外标签最长边）
 		var wH = box.width() > box.height() ? box.width() : box.height();
@@ -29,6 +39,6 @@ $(document).ready(function(){
 			height: wH,
 			left: nX,
 			top: nY
-		}).addClass("waves-effect-animation");//播放动画
-	});
-});
+		}).addClass("mmd-waves-effect-animation");//播放动画
+	}
+}
